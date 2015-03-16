99e1e79ad4fe51c710540cc9a08847ef5489e893
Derlan Lima <derlanrj@gmail.com>
2015-03-16 18:46:12 -0300

    oi

---
+++ src/android/LibraryProject/bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+C:\supersimples\app\plugins\com.phonegap.plugins.barcodescannerfront\src\android\LibraryProject\bin\AndroidManifest.xml : \
+C:\supersimples\app\plugins\com.phonegap.plugins.barcodescannerfront\src\android\LibraryProject\AndroidManifest.xml \
